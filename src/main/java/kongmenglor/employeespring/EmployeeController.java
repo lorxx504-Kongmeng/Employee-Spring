90922d4836ca24ce24c681a02574bef5d54efbc5
kongmenglor <lorxx504@umn.edu>
2023-06-02 13:47:44 -0500

    error handling

--- src/main/java/kongmenglor/employeespring/EmployeeController.java
+++ src/main/java/kongmenglor/employeespring/EmployeeController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
--- src/main/java/kongmenglor/employeespring/EmployeeService.java
+++ src/main/java/kongmenglor/employeespring/EmployeeService.java
@@ -1,6 +1,8 @@
 package kongmenglor.employeespring;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,6 +15,9 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
     public EmployeeEntity addEmployee(EmployeeEntity employee) {
+        if (employee.getEmail().isEmpty() || employee.getName().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return employeeRepo.save(employee);
     }
     public List<EmployeeEntity> findAll() {
